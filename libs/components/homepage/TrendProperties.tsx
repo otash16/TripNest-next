31a3cd29a2481e7a66cd5ff3d74342535b7ae1b4
jurabek10 <samiev.jurabek@bk.ru>
2024-11-23 23:35:00 +0900

    feat: develop functionalities  trendProperties component

--- libs/components/homepage/TrendProperties.tsx
+++ libs/components/homepage/TrendProperties.tsx
@@ -1,138 +1,6 @@
-// import React, { useState } from 'react';
-// import { useRouter } from 'next/router';
-// import { Stack, Box } from '@mui/material';
-// import useDeviceDetect from '../../hooks/useDeviceDetect';
-// import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import { Autoplay, Navigation, Pagination } from 'swiper';
-// import TopAgentCard from './TopAgentCard';
-// import { Member } from '../../types/member/member';
-// import { AgentsInquiry } from '../../types/member/member.input';
-// import { Property } from '../../types/property/property';
-// import TrendPropertyCard from './TrendPropertyCard';
-// import { PropertiesInquiry } from '../../types/property/property.input';
-// import { useQuery } from '@apollo/client';
-// import { GET_PROPERTIES } from '../../../apollo/user/query';
-// import { T } from '../../types/common';
-
-// interface TrendPropertiesProps {
-// 	initialInput: PropertiesInquiry;
-// }
-
-// const TrendProperties = (props: TrendPropertiesProps) => {
-// 	const { initialInput } = props;
-// 	const device = useDeviceDetect();
-// 	const [trendProperties, setTrendProperties] = useState<Property[]>([]);
-
-// 	/** APOLLO REQUESTS **/
-// 	const {
-// 		loading: getProperties,
-// 		data: getPropertiesData,
-// 		error: getAgentPropertiesError,
-// 		refetch: getPropertiesRefetch,
-// 	} = useQuery(GET_PROPERTIES, {
-// 		fetchPolicy: 'cache-and-network',
-// 		variables: { input: initialInput },
-// 		notifyOnNetworkStatusChange: true,
-// 		onCompleted: (data: T) => {
-// 			setTrendProperties(data?.getProperties?.list);
-// 		},
-// 	});
-// 	/** HANDLERS **/
-// 	if (trendProperties) console.log('trendProperties: +++', trendProperties);
-
-// 	if (device === 'mobile') {
-// 		return (
-// 			<Stack className={'trend-properties'}>
-// 				<Stack className={'container'}>
-// 					<Stack className={'info-box'}>
-// 						<span>Liked & Loved</span>
-// 					</Stack>
-// 					<Stack className={'wrapper'}>
-// 						<Swiper
-// 							className={'top-properties-swiper'}
-// 							slidesPerView={'auto'}
-// 							centeredSlides={true}
-// 							spaceBetween={29}
-// 							modules={[Autoplay]}
-// 						>
-// 							{trendProperties.map((property: Property) => {
-// 								return (
-// 									<SwiperSlide className={'trend-property-slide'} key={property?._id}>
-// 										<TrendPropertyCard property={property} />
-// 									</SwiperSlide>
-// 								);
-// 							})}
-// 						</Swiper>
-// 					</Stack>
-// 				</Stack>
-// 			</Stack>
-// 		);
-// 	} else {
-// 		return (
-// 			<Stack className={'trend-properties'}>
-// 				<Stack className={'container'}>
-// 					<Stack className={'info-box'}>
-// 						<Box component={'div'} className={'left'}>
-// 							<span>Liked & Loved</span>
-// 							<p>A casual, friendly option suggesting users love these places</p>
-// 						</Box>
-// 						<Box component={'div'} className={'right'}>
-// 							<div className={'more-box'}>
-// 								<span>See All Destinations</span>
-// 								<img src="/img/icons/rightup.svg" alt="" />
-// 							</div>
-// 						</Box>
-// 					</Stack>
-// 					<Stack className={'wrapper'}>
-// 						<Box component={'div'} className={'switch-btn swiper-trend-properties-prev'}>
-// 							<ArrowBackIosNewIcon />
-// 						</Box>
-// 						<Box component={'div'} className={'card-wrapper'}>
-// 							<Swiper
-// 								className={'trend-properties-swiper'}
-// 								slidesPerView={'auto'}
-// 								spaceBetween={29}
-// 								modules={[Autoplay, Navigation, Pagination]}
-// 								navigation={{
-// 									nextEl: '.swiper-trend-properties-next',
-// 									prevEl: '.swiper-trend-properties-prev',
-// 								}}
-// 							>
-// 								{trendProperties.map((property: Property) => {
-// 									return (
-// 										<SwiperSlide className={'trend-properties-slide'} key={property?._id}>
-// 											<TrendPropertyCard property={property} />
-// 										</SwiperSlide>
-// 									);
-// 								})}
-// 							</Swiper>
-// 						</Box>
-// 						<Box component={'div'} className={'switch-btn swiper-trend-properties-next'}>
-// 							<ArrowBackIosNewIcon />
-// 						</Box>
-// 					</Stack>
-// 				</Stack>
-// 			</Stack>
-// 		);
-// 	}
-// };
-
-// TrendProperties.defaultProps = {
-// 	initialInput: {
-// 		page: 1,
-// 		limit: 8,
-// 		sort: 'propertyLikes',
-// 		direction: 'DESC',
-// 		search: {},
-// 	},
-// };
-
-// export default TrendProperties;
-
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { Stack, Box } from '@mui/material';
+import { Stack, Box, IconButton, Typography } from '@mui/material';
 import useDeviceDetect from '../../hooks/useDeviceDetect';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
@@ -142,12 +10,19 @@ import TopAgentCard from './TopAgentCard';
 import { Member } from '../../types/member/member';
 import { AgentsInquiry } from '../../types/member/member.input';
 import { Property } from '../../types/property/property';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 
 import { PropertiesInquiry } from '../../types/property/property.input';
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery, useReactiveVar } from '@apollo/client';
 import { GET_PROPERTIES } from '../../../apollo/user/query';
 import { T } from '../../types/common';
 import { REACT_APP_API_URL } from '../../config';
+import { userVar } from '../../../apollo/store';
+import { Message } from '../../enums/common.enum';
+import { LIKE_TARGET_PROPERTY } from '../../../apollo/user/mutation';
+import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../sweetAlert';
+import Link from 'next/link';
 
 interface TrendPropertiesProps {
 	initialInput: PropertiesInquiry;
@@ -158,8 +33,12 @@ const TrendProperties = (props: TrendPropertiesProps) => {
 	const device = useDeviceDetect();
 	const [trendProperties, setTrendProperties] = useState<Property[]>([]);
 	const [currentIndex, setCurrentIndex] = useState(0);
+	const router = useRouter();
+	const user = useReactiveVar(userVar);
 
 	/** APOLLO REQUESTS **/
+	const [likeTargetProperty] = useMutation(LIKE_TARGET_PROPERTY);
+
 	const {
 		loading: getProperties,
 		data: getPropertiesData,
@@ -187,9 +66,43 @@ const TrendProperties = (props: TrendPropertiesProps) => {
 		}
 	};
 
+	/** HANDLERS **/
+	const likePropertyHandler = async (user: T, id: string) => {
+		try {
+			if (!id) return;
+			if (!user._id) throw new Error(Message.NOT_AUTHENTICATED);
+
+			// execute likePropertyHandler mutation
+			await likeTargetProperty({
+				variables: { input: id },
+			});
+
+			// execute getPropertiesRefetch
+			getPropertiesRefetch({ input: initialInput });
+
+			await sweetTopSmallSuccessAlert('success', 800);
+		} catch (err: any) {
+			console.log('ERROR, likePropertyHandler:', err);
+			sweetMixinErrorAlert(err.message).then;
+		}
+	};
+
+	const pushDetailhandler = async (propertyId: string) => {
+		console.log('ID;:', propertyId);
+		await router.push({ pathname: '/property/detail', query: { id: propertyId } });
+	};
+
 	useEffect(() => {
-		// Reset the index when the data is fetched or if the number of items changes
-		setCurrentIndex(0);
+		// Preserve the current index if the properties list changes
+		if (trendProperties.length > 0) {
+			setCurrentIndex((prevIndex) => {
+				const currentPropertyId = trendProperties[prevIndex]?._id;
+				const newIndex = trendProperties.findIndex((property) => property._id === currentPropertyId);
+
+				// Return the updated index or default to 0 if not found
+				return newIndex >= 0 ? newIndex : 0;
+			});
+		}
 	}, [trendProperties]);
 
 	if (device === 'mobile') {
@@ -205,7 +118,9 @@ const TrendProperties = (props: TrendPropertiesProps) => {
 							slidesPerView={'auto'}
 							centeredSlides={true}
 							spaceBetween={29}
-							modules={[Autoplay]}
+							modules={[Autoplay, Navigation, Pagination]}
+							onSlideChange={(swiper) => setCurrentIndex(swiper.realIndex)} // Update currentIndex on slide change
+							initialSlide={currentIndex}
 						>
 							{trendProperties.map((property: Property) => {
 								return (
@@ -230,7 +145,9 @@ const TrendProperties = (props: TrendPropertiesProps) => {
 						</Box>
 						<Box component={'div'} className={'right'}>
 							<div className={'more-box'}>
-								<span>See All Destinations</span>
+								<Link href={'/property'}>
+									<span>See All Categories</span>
+								</Link>
 								<img src="/img/icons/rightup.svg" alt="" />
 							</div>
 						</Box>
@@ -247,10 +164,38 @@ const TrendProperties = (props: TrendPropertiesProps) => {
 											backgroundImage: `url(${REACT_APP_API_URL}/${trendProperties[currentIndex]?.propertyImages[0]})`,
 										}}
 									>
+										<div className={'buttons'}>
+											<div className="view-like-box">
+												<IconButton color={'default'}>
+													<RemoveRedEyeIcon style={{ width: '35px', height: '35px' }} />
+												</IconButton>
+												<Typography className="view-cnt">{trendProperties[currentIndex]?.propertyViews}</Typography>
+												<IconButton
+													color={'default'}
+													onClick={() => likePropertyHandler(user, trendProperties[currentIndex]._id)}
+												>
+													{trendProperties[currentIndex]?.meLiked?.[0]?.myFavorite ? (
+														<FavoriteIcon style={{ color: 'red' }} />
+													) : (
+														<FavoriteIcon />
+													)}
+												</IconButton>
+												<Typography className="view-cnt">{trendProperties[currentIndex]?.propertyLikes}</Typography>
+											</div>
+										</div>
 										<div className="content">
 											<div className="name">{trendProperties[currentIndex]?.propertyTitle}</div>
-											<div className="des">{trendProperties[currentIndex]?.propertyDesc}</div>
-											<button>See More</button>
+											<div className="des">
+												{trendProperties[currentIndex]?.propertyDesc} dgdggdgd hshshhs shshhshsh shshhshs shshshshs
+												ssshshh shshhshhs shshshs shsssh shshhshs shshshshs ssshshh shshhshhs shshshs shsssh
+											</div>
+											<button
+												onClick={() => {
+													pushDetailhandler(trendProperties[currentIndex]?._id);
+												}}
+											>
+												See More
+											</button>
 										</div>
 									</div>
 								)}
@@ -276,7 +221,7 @@ const TrendProperties = (props: TrendPropertiesProps) => {
 TrendProperties.defaultProps = {
 	initialInput: {
 		page: 1,
-		limit: 6,
+		limit: 4,
 		sort: 'propertyLikes',
 		direction: 'DESC',
 		search: {},
