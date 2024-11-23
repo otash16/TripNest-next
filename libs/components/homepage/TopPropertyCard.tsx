0bfa4e64e8281d33fa7e5ace5ff0482e446001b5
jurabek10 <samiev.jurabek@bk.ru>
2024-11-24 00:33:38 +0900

    feat: develop topPreperties components

--- libs/components/homepage/TopPropertyCard.tsx
+++ libs/components/homepage/TopPropertyCard.tsx
@@ -1,136 +1,3 @@
-// import React from 'react';
-// import { Stack, Box, Divider, Typography } from '@mui/material';
-// import IconButton from '@mui/material/IconButton';
-// import useDeviceDetect from '../../hooks/useDeviceDetect';
-// import FavoriteIcon from '@mui/icons-material/Favorite';
-// import { Property } from '../../types/property/property';
-// import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
-// import { REACT_APP_API_URL } from '../../config';
-// import { useRouter } from 'next/router';
-// import { useReactiveVar } from '@apollo/client';
-// import { userVar } from '../../../apollo/store';
-
-// interface TopPropertyCardProps {
-// 	property: Property;
-// }
-
-// const TopPropertyCard = (props: TopPropertyCardProps) => {
-// 	const { property } = props;
-// 	const device = useDeviceDetect();
-// 	const router = useRouter();
-// 	const user = useReactiveVar(userVar);
-
-// 	/** HANDLERS **/
-
-// 	if (device === 'mobile') {
-// 		return (
-// 			<Stack className="top-card-box">
-// 				<Box
-// 					component={'div'}
-// 					className={'card-img'}
-// 					style={{ backgroundImage: `url(${REACT_APP_API_URL}/${property?.propertyImages[0]})` }}
-// 				>
-// 					<div>${property?.propertyPrice}</div>
-// 				</Box>
-// 				<Box component={'div'} className={'info'}>
-// 					<strong className={'title'}>{property?.propertyTitle}</strong>
-// 					<p className={'desc'}>{property?.propertyAddress}</p>
-// 					<div className={'options'}>
-// 						<div>
-// 							<img src="/img/icons/bed.svg" alt="" />
-// 							<span>{property?.propertyBeds} bed</span>
-// 						</div>
-// 						<div>
-// 							<img src="/img/icons/room.svg" alt="" />
-// 							<span>{property?.propertyRooms} rooms</span>
-// 						</div>
-// 						<div>
-// 							<img src="/img/icons/expand.svg" alt="" />
-// 							<span>{property?.propertySquare} m2</span>
-// 						</div>
-// 					</div>
-// 					<Divider sx={{ mt: '15px', mb: '17px' }} />
-// 					<div className={'bott'}>
-// 						<p>
-// 							{' '}
-// 							{property.propertyRent ? 'Rent' : ''} {property.propertyRent && property.propertyBarter && '/'}{' '}
-// 							{property.propertyBarter ? 'Barter' : ''}
-// 						</p>
-// 						<div className="view-like-box">
-// 							<IconButton color={'default'}>
-// 								<RemoveRedEyeIcon />
-// 							</IconButton>
-// 							<Typography className="view-cnt">{property?.propertyViews}</Typography>
-// 							<IconButton color={'default'}>
-// 								{property?.meLiked && property?.meLiked[0]?.myFavorite ? (
-// 									<FavoriteIcon style={{ color: 'red' }} />
-// 								) : (
-// 									<FavoriteIcon />
-// 								)}
-// 							</IconButton>
-// 							<Typography className="view-cnt">{property?.propertyLikes}</Typography>
-// 						</div>
-// 					</div>
-// 				</Box>
-// 			</Stack>
-// 		);
-// 	} else {
-// 		return (
-// 			<Stack className="top-card-box">
-// 				<Box
-// 					component={'div'}
-// 					className={'card-img'}
-// 					style={{ backgroundImage: `url(${REACT_APP_API_URL}/${property?.propertyImages[0]})` }}
-// 				>
-// 					<div>${property?.propertyPrice}</div>
-// 				</Box>
-// 				<Box component={'div'} className={'info'}>
-// 					<strong className={'title'}>{property?.propertyTitle}</strong>
-// 					<p className={'desc'}>{property?.propertyAddress}</p>
-// 					<div className={'options'}>
-// 						<div>
-// 							<img src="/img/icons/bed.svg" alt="" />
-// 							<span>{property?.propertyBeds} bed</span>
-// 						</div>
-// 						<div>
-// 							<img src="/img/icons/room.svg" alt="" />
-// 							<span>{property?.propertyRooms} rooms</span>
-// 						</div>
-// 						<div>
-// 							<img src="/img/icons/expand.svg" alt="" />
-// 							<span>{property?.propertySquare} m2</span>
-// 						</div>
-// 					</div>
-// 					<Divider sx={{ mt: '15px', mb: '17px' }} />
-// 					<div className={'bott'}>
-// 						<p>
-// 							{' '}
-// 							{property.propertyRent ? 'Rent' : ''} {property.propertyRent && property.propertyBarter && '/'}{' '}
-// 							{property.propertyBarter ? 'Barter' : ''}
-// 						</p>
-// 						<div className="view-like-box">
-// 							<IconButton color={'default'}>
-// 								<RemoveRedEyeIcon />
-// 							</IconButton>
-// 							<Typography className="view-cnt">{property?.propertyViews}</Typography>
-// 							<IconButton color={'default'}>
-// 								{property?.meLiked && property?.meLiked[0]?.myFavorite ? (
-// 									<FavoriteIcon style={{ color: 'red' }} />
-// 								) : (
-// 									<FavoriteIcon />
-// 								)}
-// 							</IconButton>
-// 							<Typography className="view-cnt">{property?.propertyLikes}</Typography>
-// 						</div>
-// 					</div>
-// 				</Box>
-// 			</Stack>
-// 		);
-// 	}
-// };
-
-// export default TopPropertyCard;
-
 import React from 'react';
 import { Stack, Box, Divider, Typography } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
@@ -153,6 +20,10 @@ const TopPropertyCard = (props: TopPropertyCardProps) => {
 	const user = useReactiveVar(userVar);
 
 	/** HANDLERS **/
+	const pushDetailhandler = async (propertyId: string) => {
+		console.log('ID;:', propertyId);
+		await router.push({ pathname: '/property/detail', query: { id: propertyId } });
+	};
 	if (device === 'mobile') {
 		return (
 			<Stack className="top-card-box">
@@ -160,15 +31,10 @@ const TopPropertyCard = (props: TopPropertyCardProps) => {
 					component={'div'}
 					className={'card-img'}
 					style={{ backgroundImage: `url(${REACT_APP_API_URL}/${property?.propertyImages[0]})` }}
+					onClick={() => {
+						pushDetailhandler(property._id);
+					}}
 				>
-					{property?.propertyRank && property?.propertyRank >= 50 ? (
-						<div className={'status'}>
-							<img src="/img/icons/electricity.svg" alt="" />
-							<span>top</span>
-						</div>
-					) : (
-						''
-					)}
 					<div className={'price'}>${property.propertyPrice}</div>
 				</Box>
 				<Box component={'div'} className={'info'}>
@@ -207,7 +73,12 @@ const TopPropertyCard = (props: TopPropertyCardProps) => {
 		);
 	} else {
 		return (
-			<Stack className="top-card-box">
+			<Stack
+				className="top-card-box"
+				onClick={() => {
+					pushDetailhandler(property._id);
+				}}
+			>
 				<Box
 					component={'div'}
 					className={'card-img'}
