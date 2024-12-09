49003cd87a791a249fa262c9bc77777e158fecf8
jurabek10 <samiev.jurabek@bk.ru>
2024-12-09 22:42:30 +0900

    feat: develop second stage of Chatting system

--- apollo/client.ts
+++ apollo/client.ts
@@ -28,6 +28,29 @@ const tokenRefreshLink = new TokenRefreshLink({
 	},
 });
 
+// CustomWebSocket Client
+class LoggingWebSocket {
+	private socket: WebSocket;
+	constructor(url: string) {
+		this.socket = new WebSocket(url);
+		this.socket.onopen = () => {
+			console.log('WebSocket connection!');
+		};
+		this.socket.onmessage = (msg) => {
+			console.log('WebSocket message: ', msg.data);
+		};
+		this.socket.onerror = (error) => {
+			console.log('WebSocket error: ', error);
+		};
+	}
+	send(data: string | ArrayBuffer | SharedArrayBuffer | Blob | ArrayBufferView) {
+		this.socket.send(data);
+	}
+	close() {
+		this.socket.close();
+	}
+}
+
 function createIsomorphicLink() {
 	if (typeof window !== 'undefined') {
 		const authLink = new ApolloLink((operation, forward) => {
@@ -56,6 +79,7 @@ function createIsomorphicLink() {
 					return { headers: getHeaders() };
 				},
 			},
+			webSocketImpl: LoggingWebSocket,
 		});
 
 		const errorLink = onError(({ graphQLErrors, networkError, response }) => {
